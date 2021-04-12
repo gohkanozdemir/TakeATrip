207860585e845189991736345260d007b9d14ed6
Gokhan Ozdemir <gokhanozdemir2@outlook.com>
2021-04-12 23:27:51 +0200

    missing Core

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+
+{   //class : referans tip olabilirlik
+    // IEntity : IEntity ve/veya IEntity implemente eden bir nesne
+    // new(): constructor i olanlar 
+    public interface IEntityRepository<T> where T:class,IEntity,new()   // Generic constrait
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter); 
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
